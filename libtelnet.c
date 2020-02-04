7bff39df5768351174dca219e3c79ebb58b6b9ac
Xiang Xiao <xiaoxiang@xiaomi.com>
2020-02-04 15:47:31 +0800

    Fix some wrong state handler in _negotiate
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

--- libtelnet.c
+++ libtelnet.c
@@ -407,7 +407,6 @@ static void _negotiate(telnet_t *telnet, unsigned char telopt) {
 			break;
 		case Q_WANTNO_OP:
 			_set_rfc1143(telnet, telopt, Q_US(q), Q_YES);
-			NEGOTIATE_EVENT(telnet, TELNET_EV_WILL, telopt);
 			_error(telnet, __LINE__, __func__, TELNET_EPROTOCOL, 0,
 					"DONT answered by WILL");
 			break;
@@ -437,7 +436,8 @@ static void _negotiate(telnet_t *telnet, unsigned char telopt) {
 			break;
 		case Q_WANTNO_OP:
 			_set_rfc1143(telnet, telopt, Q_US(q), Q_WANTYES);
-			NEGOTIATE_EVENT(telnet, TELNET_EV_DO, telopt);
+			_send_negotiate(telnet, TELNET_DO, telopt);
+			NEGOTIATE_EVENT(telnet, TELNET_EV_WONT, telopt);
 			break;
 		case Q_WANTYES:
 		case Q_WANTYES_OP:
@@ -465,7 +465,6 @@ static void _negotiate(telnet_t *telnet, unsigned char telopt) {
 			break;
 		case Q_WANTNO_OP:
 			_set_rfc1143(telnet, telopt, Q_YES, Q_HIM(q));
-			NEGOTIATE_EVENT(telnet, TELNET_EV_DO, telopt);
 			_error(telnet, __LINE__, __func__, TELNET_EPROTOCOL, 0,
 					"WONT answered by DO");
 			break;
@@ -491,12 +490,12 @@ static void _negotiate(telnet_t *telnet, unsigned char telopt) {
 			break;
 		case Q_WANTNO:
 			_set_rfc1143(telnet, telopt, Q_NO, Q_HIM(q));
-			NEGOTIATE_EVENT(telnet, TELNET_EV_WONT, telopt);
+			NEGOTIATE_EVENT(telnet, TELNET_EV_DONT, telopt);
 			break;
 		case Q_WANTNO_OP:
 			_set_rfc1143(telnet, telopt, Q_WANTYES, Q_HIM(q));
 			_send_negotiate(telnet, TELNET_WILL, telopt);
-			NEGOTIATE_EVENT(telnet, TELNET_EV_WILL, telopt);
+			NEGOTIATE_EVENT(telnet, TELNET_EV_DONT, telopt);
 			break;
 		case Q_WANTYES:
 		case Q_WANTYES_OP:
